7c3e612a6f26e1d86eb720d7e382d32bc4780519
sukanta7803 <setudeyindia@gmail.com>
2025-09-21 11:53:44 +0530

    third-commit

--- python_backend/app.py
+++ python_backend/app.py
@@ -244,7 +244,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    host = os.environ.get('FLASK_HOST', '127.0.0.1')
-    port = int(os.environ.get('FLASK_PORT', '5000'))
+    host = os.environ.get('FLASK_HOST', '0.0.0.0')  # use 0.0.0.0 so Render can access it
+    port = int(os.environ.get('FLASK_PORT') or os.environ.get('PORT', '5000'))
     debug = os.environ.get('FLASK_DEBUG', '0') == '1'
     app.run(host=host, port=port, debug=debug)
